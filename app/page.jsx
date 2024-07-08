779b13c04a1dabed9cd891190c109bf5bd3da0f6
brilliahib <brilliahib21@gmail.com>
2024-07-09 00:58:05 +0700

    fix: navbar and font

--- app/page.jsx
+++ app/page.jsx
@@ -1,19 +1,20 @@
+import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
 
 export default function Home() {
   return (
     <>
-      <div className="bg-[#0E1333] h-screen flex flex-col gap-4 justify-center items-center">
+      <div className="bg-[#0E1333] h-screen flex flex-col space-y-8 justify-center items-center">
         <div className="text-white text-6xl text-center font-semibold -mt-36 font-orbitron">
           <p>
-            Break the <a className="text-[#FFA62D]">algorithm</a>,
+            Break the <a className="text-primary">algorithm</a>,
           </p>
           <p>code the future</p>
         </div>
-        <button className="flex flex-row gap-2 bg-[#FFA62D] p-2 rounded-md font-semibold text-black">
+        <Button className="flex flex-row gap-2 rounded-md font-bold text-black">
           Get Started
           <ArrowRight color="#000000" />
-        </button>
+        </Button>
       </div>
     </>
   );
---
+++ components/navbar/nav-mobile.jsx
@@ -0,0 +1,71 @@
+import Link from "next/link";
+
+const NavMobile = () => {
+  return (
+    <>
+      <nav className="bg-[#080f42] w-full md:hidden block fixed bottom-0 pad-x py-4">
+        <div className="text-white justify-around items-center flex w-full text-sm">
+          <Link href="/">
+            <div className="flex flex-col justify-center items-center gap-y-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                />
+              </svg>
+              Home
+            </div>
+          </Link>
+          <Link href="/lomba">
+            <div className="flex flex-col justify-center items-center gap-y-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M16.5 18.75h-9m9 0a3 3 0 0 1 3 3h-15a3 3 0 0 1 3-3m9 0v-3.375c0-.621-.503-1.125-1.125-1.125h-.871M7.5 18.75v-3.375c0-.621.504-1.125 1.125-1.125h.872m5.007 0H9.497m5.007 0a7.454 7.454 0 0 1-.982-3.172M9.497 14.25a7.454 7.454 0 0 0 .981-3.172M5.25 4.236c-.982.143-1.954.317-2.916.52A6.003 6.003 0 0 0 7.73 9.728M5.25 4.236V4.5c0 2.108.966 3.99 2.48 5.228M5.25 4.236V2.721C7.456 2.41 9.71 2.25 12 2.25c2.291 0 4.545.16 6.75.47v1.516M7.73 9.728a6.726 6.726 0 0 0 2.748 1.35m8.272-6.842V4.5c0 2.108-.966 3.99-2.48 5.228m2.48-5.492a46.32 46.32 0 0 1 2.916.52 6.003 6.003 0 0 1-5.395 4.972m0 0a6.726 6.726 0 0 1-2.749 1.35m0 0a6.772 6.772 0 0 1-3.044 0"
+                />
+              </svg>
+              Lomba
+            </div>
+          </Link>
+          <Link href="/seminar">
+            <div className="flex flex-col justify-center items-center gap-y-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15.59 14.37a6 6 0 0 1-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 0 0 6.16-12.12A14.98 14.98 0 0 0 9.631 8.41m5.96 5.96a14.926 14.926 0 0 1-5.841 2.58m-.119-8.54a6 6 0 0 0-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 0 0-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 0 1-2.448-2.448 14.9 14.9 0 0 1 .06-.312m-2.24 2.39a4.493 4.493 0 0 0-1.757 4.306 4.493 4.493 0 0 0 4.306-1.758M16.5 9a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z"
+                />
+              </svg>
+              Seminar
+            </div>
+          </Link>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default NavMobile;
