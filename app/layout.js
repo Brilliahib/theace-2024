95680b2fc8cd3b925ada6dfae722cb4fc70609f1
princeofverry <verrykurniawan@students.undip.ac.id>
2024-07-08 21:28:41 +0700

    fix: folder navbar

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "./navbar/navbar";
+import Navbar from "../components/navbar/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
