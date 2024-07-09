1616ae80420b1e20c52272a15d7caef794349b69
brilliahib <brilliahib21@gmail.com>
2024-07-09 20:25:34 +0700

    add: faq + fix: bg-image

--- app/page.jsx
+++ app/page.jsx
@@ -1,17 +1,19 @@
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
 import bg from "/public/images/bg-particle.png";
+import { Orbitron } from "next/font/google";
+
+const orbitron = Orbitron({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-      <div
-        className="md:h-screen min-h-[70vh] flex flex-col space-y-8 justify-center items-center bg-cover bg-center"
-        style={{ backgroundImage: `url(${bg.src})` }}
-      >
-        <div className="text-white text-4xl md:text-6xl text-center font-semibold font-orbitron">
+      <div className="md:h-screen min-h-[70vh] flex flex-col space-y-8 justify-center items-center">
+        <div
+          className={`text-white text-4xl md:text-6xl text-center font-semibold font-orbitron ${orbitron.className}`}
+        >
           <p>
-            Break the <a className="text-primary">algorithm</a>,
+            Break the <span className="text-primary">algorithm</span>,
           </p>
           <p>code the future</p>
         </div>
