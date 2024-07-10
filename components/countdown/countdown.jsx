95f4735ec07e990c81c84a0b840a3c7d83371f36
brilliahib <brilliahib21@gmail.com>
2024-07-10 16:39:57 +0700

    add: seconds on countdown + fix: navbar (change text size)

--- components/countdown/countdown.jsx
+++ components/countdown/countdown.jsx
@@ -11,41 +11,59 @@ const Countdown = ({ targetDate }) => {
         days: Math.floor(difference / (1000 * 60 * 60 * 24)),
         hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
         minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
       };
     }
-    return { days: 0, hours: 0, minutes: 0 };
+    return { days: 0, hours: 0, minutes: 0, seconds: 0 };
   };
 
-  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+  const [timeLeft, setTimeLeft] = useState({});
+  const [hasMounted, setHasMounted] = useState(false);
+
+  useEffect(() => {
+    setTimeLeft(calculateTimeLeft());
+    setHasMounted(true);
+  }, [targetDate]);
 
   useEffect(() => {
     const timer = setInterval(() => {
       setTimeLeft(calculateTimeLeft());
-    }, 60000); // Update every minute
+    }, 1000); // Update every second
 
     return () => clearInterval(timer);
   }, [targetDate]);
 
+  if (!hasMounted) {
+    return null; // Render nothing on the server side
+  }
+
   return (
-    <div className="text-white flex items-center gap-x-4">
+    <div className="text-white flex items-center md:gap-x-4 gap-x-2">
       <div className="flex flex-col justify-center items-center space-y-2">
-        <h1 className="text-lg font-bold">Days</h1>
-        <div className="border-t-2 border-l-2 border-4 border-primary p-4 rounded-xl font-bold text-xl">
-          {timeLeft.days}
+        <h1 className="text-base font-bold">Days</h1>
+        <div className="border-t-2 border-l-2 border-4 border-primary p-4 rounded-xl font-bold w-full flex justify-center">
+          <p className="md:text-xl text-base">{timeLeft.days}</p>
         </div>
       </div>
-      <span>:</span>
+      <div className="text-xl font-bold mt-6">:</div>
       <div className="flex flex-col justify-center items-center space-y-2">
-        <h1 className="text-lg font-bold">Hours</h1>
-        <div className="border-t-2 border-l-2 border-4 border-primary p-4 rounded-xl font-bold text-xl">
-          {timeLeft.hours}
+        <h1 className="text-base font-bold">Hours</h1>
+        <div className="border-t-2 border-l-2 border-4 border-primary p-4 rounded-xl font-bold w-full flex justify-center">
+          <p className="md:text-xl text-base">{timeLeft.hours}</p>
         </div>
       </div>
-      <span>:</span>
+      <div className="text-xl font-bold mt-6">:</div>
       <div className="flex flex-col justify-center items-center space-y-2">
-        <h1 className="text-lg font-bold">Minutes</h1>
-        <div className="border-t-2 border-l-2 border-4 border-primary p-4 rounded-xl font-bold text-xl">
-          {timeLeft.minutes}
+        <h1 className="text-base font-bold">Minutes</h1>
+        <div className="border-t-2 border-l-2 border-4 border-primary p-4 rounded-xl font-bold w-full flex justify-center">
+          <p className="md:text-xl text-base">{timeLeft.minutes}</p>
+        </div>
+      </div>
+      <div className="text-xl font-bold mt-6">:</div>
+      <div className="flex flex-col justify-center items-center space-y-2">
+        <h1 className="text-base font-bold">Seconds</h1>
+        <div className="border-t-2 border-l-2 border-4 border-primary p-4 rounded-xl font-bold w-full flex justify-center">
+          <p className="md:text-xl text-base">{timeLeft.seconds}</p>
         </div>
       </div>
     </div>
