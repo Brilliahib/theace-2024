fed139a4966a1393ab713e3d960d372b95988a9f
brilliahib <brilliahib21@gmail.com>
2024-07-11 16:42:13 +0700

    add: docum image + fix: padding on navbar

--- components/documentation/documentation.jsx
+++ components/documentation/documentation.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import MiniPicture from './min-pic'
+import React from "react";
+import MiniPicture from "./min-pic";
 
 const Documentation = () => {
-    return (
-        <div>
-            <div className="text-center mt-16">
-                <h1 className="md:text-3xl text-xl font-bold">Dokumentasi Tahun Lalu</h1>
-                <h3 className="text-lg">Berikut keseruan The ACE tahun lalu</h3>
-            </div>
-            <MiniPicture />
-        </div>
-    )
-}
+  return (
+    <div>
+      <div className="text-center space-y-4 mt-24">
+        <h1 className="text-3xl font-bold">Dokumentasi Tahun Lalu</h1>
+        <p className="text-[#DCE2F1]">Berikut keseruan The ACE tahun lalu</p>
+      </div>
+      <MiniPicture />
+    </div>
+  );
+};
 
-export default Documentation
+export default Documentation;
