77b8319187bb5fb957637ae696971232bf99727c
princeofverry <verrykurniawan@students.undip.ac.id>
2024-07-24 11:18:43 +0700

    feat: tambahin component lomba pada landing page

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,6 @@
 import Documentation from "@/components/documentation/documentation";
 import EventComponent from "@/components/event/event";
+import LombaPage from "@/components/lomba/lomba";
 import Marquee from "@/components/marquee/marquee-photo";
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
@@ -10,18 +11,21 @@ export default function Home() {
   return (
     <>
       <div
-        className="md:h-screen min-h-[70vh] flex flex-col space-y-8 justify-center items-start bg-cover bg-center pad-x"
-        style={{ backgroundImage: `url(${bgLanding.src})` }}
+        className="md:h-screen min-h-[85vh] flex flex-col space-y-8 justify-start sm:justify-center items-start bg-cover bg-center pad-x"
+        style={{
+          backgroundImage: `url(${bgLanding.src})`,
+
+        }}
       >
         <div
-          className={`text-white text-2xl md:text-4xl space-y-4 text-left font-semibold font-boomBox2`}
+          className={`text-white text-xl md:text-4xl md:space-y-4 text-left font-semibold font-boomBox2`}
         >
           <p>
             Break the <span className="text-[#FF29BB]">algorithm</span>,
           </p>
           <p>code the future</p>
         </div>
-        <p className="w-1/2 text-pretty">
+        <p className="md:w-1/2 w-5/6 text-pretty md:text-base text-sm">
           Kami mengundang kalian semua untuk bergabung dalam serangkaian lomba seru dan menantang yang kami adakan! Apakah kamu siap untuk mengasah kemampuan dan menunjukkan bakatmu? Inilah saat yang tepat untuk berpartisipasi
         </p>
         <Button className="flex flex-row gap-2 rounded-md font-bold text-black h-12">
@@ -30,8 +34,7 @@ export default function Home() {
         </Button>
       </div>
       <Marquee />
-      <EventComponent />
-      <Documentation />
+      <LombaPage />
     </>
   );
 }
