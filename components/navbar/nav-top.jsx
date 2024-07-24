2b6dd062323647a291db449168521d984a73ca6b
brilliahib <brilliahib21@gmail.com>
2024-07-24 11:19:59 +0700

    fix: nav-mobile + nav-top

--- components/navbar/nav-top.jsx
+++ components/navbar/nav-top.jsx
@@ -6,15 +6,17 @@ import { Phone } from "lucide-react";
 const NavTop = () => {
   return (
     <>
-      <div className="w-full bg-transparent pad-x py-1 md:hidden block backdrop-filter backdrop-blur-lg">
+      <div className="w-full sticky top-0 bg-secondary pad-x py-3 md:hidden block backdrop-filter backdrop-blur-lg z-50">
         <div className="flex justify-between items-center">
           <div className="flex flex-row items-center">
             {/* masupin gambar the ace */}
             <Image src={logo} alt="Logo" width={80} height={80} />
           </div>
           <div>
-            <Button className="flex flex-row gap-2 rounded-md font-bold text-black">
-              <Phone size={20} fill="#000000" stroke="#000000" />
+            <Button
+              variant="outline"
+              className="border p-5 flex text-primary bg-secondary rounded-full"
+            >
               Contact
             </Button>
           </div>
