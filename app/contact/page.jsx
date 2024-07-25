095e9fc8dd07124556c41babeb8c1f748d11ac3c
Dzakiamriz22 <dzakiamriz12@gmail.com>
2024-07-25 09:25:47 +0700

    fix : hover + link contact-card

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,89 +1,92 @@
+import Link from 'next/link';
 import ContactCard from "@/components/contact/contact-card";
 
-const ContactPage = () => {
-  return (
-    <>
-      {/* Contact Section */}
-      <div className="md:min-h-[350px] min-h-[200px] bg-secondary pad-x flex items-center">
-        <div className="space-y-4 md:text-left text-center md:mt-12">
-          <h1 className="md:text-4xl text-2xl font-bold uppercase font-boomBox2">
-            CONTACT
-          </h1>
-          <p className="text-base mt-4">
-            Tanyakan atau sampaikan pesan Anda kepada kami melalui halaman ini.
-            Kami siap membantu!
-          </p>
-        </div>
-      </div>
-
-      {/* Content Sections */}
-      <div className="flex flex-col items-center justify-center px-4 sm:px-8 lg:px-16 mt-16">
-        {/* UI/UX Design Section */}
-        <div className="text-center font-bold mb-4 text-white">Narahubung</div>
-        <div className="text-center font-bold text-2xl text-primary mb-4">
-          UI/UX Design
-        </div>
-        <div className="flex justify-center flex-wrap gap-8 pad-x mb-16">
-          <ContactCard
-            role="UI/UX Design"
-            name="Mutiara Sabrina R."
-            phone="+62 85803228042"
-          />
-          <ContactCard
-            role="UI/UX Design"
-            name="Edi Wicoro"
-            phone="+62 82325720215"
-          />
-        </div>
-
-        {/* Essay Section */}
-        <div className="text-center font-bold mb-4 text-white">Narahubung</div>
-        <div className="text-center font-bold text-2xl text-primary mb-4">
-          Essay
-        </div>
-        <div className="flex justify-center flex-wrap gap-8 pad-x mb-16">
-          <ContactCard
-            role="Essay"
-            name="Wahyu Ramadhani"
-            phone="+62 82241992151"
-          />
-          <ContactCard
-            role="Essay"
-            name="Daffa Ananda"
-            phone="+62 81946767250"
-          />
-          <ContactCard
-            role="Essay"
-            name="Syada Saleha"
-            phone="+62 82352460763"
-          />
-        </div>
-
-        {/* Capture The Flag Section */}
-        <div className="text-center font-bold mb-4">Narahubung</div>
-        <div className="text-center font-bold text-2xl text-primary mb-4">
-          Capture The Flag
-        </div>
-        <div className="flex justify-center flex-wrap gap-8 pad-x">
-          <ContactCard
-            role="Capture The Flag"
-            name="Anastasya"
-            phone="+62 83822629004"
-          />
-          <ContactCard
-            role="Capture The Flag"
-            name="Tian Putra"
-            phone="+62 8976666609"
-          />
-          <ContactCard
-            role="Capture The Flag"
-            name="Shulhan Aziz"
-            phone="+62 83111147273"
-          />
-        </div>
-      </div>
-    </>
-  );
+const contacts = {
+  "UI/UX Design": [
+    { name: "Mutiara Sabrina R.", phone: "+62 85803228042" },
+    { name: "Edi Wicoro", phone: "+62 82325720215" }
+  ],
+  "Essay": [
+    { name: "Wahyu Ramadhani", phone: "+62 82241992151" },
+    { name: "Daffa Ananda", phone: "+62 81946767250" },
+    { name: "Syada Saleha", phone: "+62 82352460763" }
+  ],
+  "Capture The Flag": [
+    { name: "Anastasya", phone: "+62 83822629004" },
+    { name: "Tian Putra", phone: "+62 8976666609" },
+    { name: "Shulhan Aziz", phone: "+62 83111147273" }
+  ]
 };
+
+const ContactPage = () => (
+  <>
+    {/* Contact Section */}
+    <div className="md:min-h-[350px] min-h-[200px] bg-secondary pad-x flex items-center">
+      <div className="space-y-4 md:text-left text-center md:mt-12">
+        <h1 className="md:text-4xl text-2xl font-normal uppercase font-boomBox2">
+          CONTACT
+        </h1>
+        <p className="text-base mt-4">
+          Tanyakan atau sampaikan pesan Anda kepada kami melalui halaman ini.
+          Kami siap membantu!
+        </p>
+      </div>
+    </div>
+
+    {/* Content Sections */}
+    <div className="flex flex-col items-center justify-center px-4 sm:px-8 lg:px-16 mt-16">
+      {/* UI/UX Design Section */}
+      <div className="text-center font-bold mb-4 text-white">Narahubung</div>
+      <div className="text-center font-bold text-2xl text-primary mb-4">
+        UI/UX Design
+      </div>
+      <div className="flex justify-center flex-wrap gap-8 pad-x mb-16">
+        {contacts["UI/UX Design"].map(contact => (
+          <Link href={`https://wa.me/${contact.phone.replace(/\D/g, '')}`} key={contact.phone}>
+            <ContactCard
+              role="UI/UX Design"
+              name={contact.name}
+              phone={contact.phone}
+            />
+          </Link>
+        ))}
+      </div>
+
+      {/* Essay Section */}
+      <div className="text-center font-bold mb-4 text-white">Narahubung</div>
+      <div className="text-center font-bold text-2xl text-primary mb-4">
+        Essay
+      </div>
+      <div className="flex justify-center flex-wrap gap-8 pad-x mb-16">
+        {contacts["Essay"].map(contact => (
+          <Link href={`https://wa.me/${contact.phone.replace(/\D/g, '')}`} key={contact.phone}>
+            <ContactCard
+              role="Essay"
+              name={contact.name}
+              phone={contact.phone}
+            />
+          </Link>
+        ))}
+      </div>
+
+      {/* Capture The Flag Section */}
+      <div className="text-center font-bold mb-4">Narahubung</div>
+      <div className="text-center font-bold text-2xl text-primary mb-4">
+        Capture The Flag
+      </div>
+      <div className="flex justify-center flex-wrap gap-8 pad-x">
+        {contacts["Capture The Flag"].map(contact => (
+          <Link href={`https://wa.me/${contact.phone.replace(/\D/g, '')}`} key={contact.phone}>
+            <ContactCard
+              role="Capture The Flag"
+              name={contact.name}
+              phone={contact.phone}
+            />
+          </Link>
+        ))}
+      </div>
+    </div>
+  </>
+);
 
 export default ContactPage;
--- components/contact/contact-card.jsx
+++ components/contact/contact-card.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import { Card } from '../ui/card';
 
-export default function ContactCard({ role, name, phone }) {
+export default function ContactCard({ name, phone }) {
   return (
-    <Card className="bg-[#371A6C] text-white w-full sm:w-80 shadow-lg border-[#FFA62D] p-4 relative cursor-pointer">
-      <div className="flex items-center">
-        <div className="bg-gray-400 w-16 h-16 rounded-full"></div>
-        <div className="ml-4 flex flex-col justify-start">
-          <p className="font-bold">{name}</p>
-          <p className="mt-1 text-primary">{phone}</p>
+    <Card className="bg-[#371A6C] text-white w-full sm:w-80 shadow-lg p-4 relative cursor-pointer hover:border-primary border-2 border-transparent">
+      <div className="flex items-start justify-between">
+        <div className="flex items-center">
+          <div className="bg-gray-400 w-16 h-16 rounded-full"></div>
+          <div className="ml-4 flex flex-col justify-start">
+            <p className="font-bold">{name}</p>
+            <p className="mt-1 text-primary">{phone}</p>
+          </div>
         </div>
-      </div>
-      <div className="absolute top-4 right-4">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-primary">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
-        </svg>
+        <div className="ml-4">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-primary">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+          </svg>
+        </div>
       </div>
     </Card>
   );
