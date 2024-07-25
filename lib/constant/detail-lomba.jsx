9156f08ce5b85d5851ad22585e19c6ec4e431b7a
brilliahib <brilliahib21@gmail.com>
2024-07-25 21:19:18 +0700

    fix: change detail lomba

--- lib/constant/detail-lomba.jsx
+++ lib/constant/detail-lomba.jsx
@@ -6,8 +6,8 @@ const detailLomba = [
       "UI/UX Design adalah perlombaan tingkat nasional yang diadakan oleh program studi Teknik Komputer Universitas Diponegoro. Dengan tema “Safe Cyberspace : Eliminate the threats, to build safer future”, lomba bertujuan untuk mendorong kreativitas dan menginspirasi solusi desain cerdas dan futuristik. Harapannya peserta dapat menghadirkan ide-ide revolusioner yang memanfaatkan AI untuk menciptakan pengalaman pengguna yang luar biasa dan bermanfaat.",
     theme:
       "Design the Impossible for Future: Innovate UI/UX with Artificial Intelligence (AI)",
-    price: "60.000",
-    targetDate: "2024-08-01T07:00:00",
+    price: "70.000",
+    targetDate: "2024-08-10T23:59:59",
     timeline: [
       { title: "Pendaftaran Gelombang 1", date: "01 - 10 Agustus 2024" },
       { title: "Pendaftaran Gelombang 2", date: "11 - 20 Agustus 2024" },
@@ -27,7 +27,7 @@ const detailLomba = [
       "Lomba Capture The Flag (CTF) ini merupakan perlombaan tingkat nasional yang diselenggarakan oleh program studi Teknik Komputer Universitas Diponegoro. Lomba ini menguji keterampilan peserta dalam keamanan siber melalui berbagai tantangan hacking dan pemecahan masalah. Dengan tema “Safe Cyberspace: Eliminate the threats, to build safer future”, lomba ini bertujuan mengasah kemampuan peserta dalam mengidentifikasi dan mengatasi ancaman siber. Harapannya peserta dapat menunjukkan keterampilan mereka dalam melindungi ruang siber dan berkontribusi pada upaya menciptakan masa depan yang lebih aman dan terlindungi.",
     theme: "Safe Cyberspace : Eliminate the Threats, to Build Safer Future",
     price: "60.000",
-    targetDate: "2024-08-01T07:00:00",
+    targetDate: "2024-08-10T23:59:59",
     timeline: [
       { title: "Pendaftaran Gelombang 1", date: "01 - 10 Agustus 2024" },
       { title: "Pendaftaran Gelombang 2", date: "11 - 20 Agustus 2024" },
@@ -46,8 +46,8 @@ const detailLomba = [
     description:
       "Lomba essay ini adalah kompetisi tingkat nasional yang diadakan oleh program studi Teknik Komputer Universitas Diponegoro. Dengan tema “The Role of IoT in Achieving Sustainable Development Goals”, lomba ini bertujuan untuk mengeksplorasi solusi inovatif dan dampak positif IoT terhadap keberlanjutan global. Harapannya peserta dapat menyajikan tulisan yang mendalam, inspiratif, dan memberikan wawasan baru tentang bagaimana teknologi dapat mendukung pencapaian tujuan berkelanjutan.",
     theme: "The Role of IoT in Achieving Sustainable Development Goals",
-    price: "60.000",
-    targetDate: "2024-08-01T07:00:00",
+    price: "45.000",
+    targetDate: "2024-08-10T23:59:59",
     timeline: [
       { title: "Pendaftaran Gelombang 1", date: "01 - 10 Agustus 2024" },
       { title: "Pendaftaran Gelombang 2", date: "11 - 20 Agustus 2024" },
